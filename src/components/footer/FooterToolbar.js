74f46606dfa3dd762fa92911d26306dd65f3709b
Steven Meads <sbmeads@gmail.com>
2017-05-10 14:06:32 -0700

    add header footer components

---
+++ src/components/footer/FooterToolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (props) => {
+  return(
+    <footer className="toolbar toolbar-footer">
+      <div className="toolbar-actions">
+        <button className="btn btn-primary pull-right">
+          Save
+        </button>
+      </div>
+    </footer>
+  );
+}
