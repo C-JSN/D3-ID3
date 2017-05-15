5e632c21c9539d96c02c44df614116cb2be102aa
Christian Pascual <christian@Christians-MacBook-Pro.local>
2017-05-15 12:47:51 -0700

    connected width to staore

--- src/actions/ScatterPlotActions.js
+++ src/actions/ScatterPlotActions.js
@@ -1,4 +1,5 @@
 export const SCATTER_PLOT = "SCATTER_PLOT";
+export const UPDATE_WIDTH = "UPDATE_WIDTH";
 
 export const getScatterPlot = (payload) => dispatch => {
   dispatch({
@@ -6,3 +7,19 @@ export const getScatterPlot = (payload) => dispatch => {
     payload: payload
   });
 };
+
+// export const updateWidth = (width) => dispatch =>{
+//   console.log('')
+//   dispatch({
+//     type: UPDATE_WIDTH,
+//     width
+//   });
+// }
+
+export const updateWidth =  (width) => {
+  return {
+    type: UPDATE_WIDTH,
+    width
+  }
+}
+
