b1a8fffef136eb105c0625b2309e7b7aea54c781
eveafeline <eveafeline@gmail.com>
2017-05-18 14:53:43 -0700

    pop out webview

--- src/components/footer/FooterToolbar.js
+++ src/components/footer/FooterToolbar.js
@@ -4,9 +4,12 @@ export default (props) => {
   return(
     <footer className="toolbar toolbar-footer">
       <div className="toolbar-actions">
-        <button className="btn btn-primary pull-right">
-          Save
-        </button>
+        <div style={{backgroundColor: '#333', width: "80%", height: "80%", display: 'inline'}}>
+          information
+        </div>
+        // <button className="btn btn-primary pull-right">
+        //   Save
+        // </button>
       </div>
     </footer>
   );
