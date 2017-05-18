53cb23701318aa7cdee2d3442601d968c29f944d
Steven Meads <sbmeads@gmail.com>
2017-05-18 11:14:27 -0700

    remove d3parser btn in main header

--- src/components/header/HeaderToolbar.js
+++ src/components/header/HeaderToolbar.js
@@ -38,10 +38,6 @@ export default (props) => {
           <span className="icon icon-upload icon-text"></span>
           Export
         </a>
-        <button className="btn btn-default" id="d3parser" onClick={(e)=>getD3ParserObj()}>
-          <span className="icon icon-login icon-text"></span>
-          D3 Parser
-        </button>
         <div className="dropdown">
           <button className="btn btn-default btn-dropdown" onClick={toggleDropDown}>
             <span className="icon icon-layout icon-text"></span>
