ba182bd9ab899b3d481740b0d5c74c597c762339
Steven Meads <sbmeads@gmail.com>
2017-05-19 17:49:33 -0700

    adjusted toolbar min-height

--- src/components/footer/FooterToolbar.js
+++ src/components/footer/FooterToolbar.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 export default (props) => {
   return(
-    <footer className="toolbar toolbar-footer">
-      <div className="toolbar-actions">
-        <div style={{backgroundColor: '#333', width: "80%", height: "80%", display: 'inline'}}>
-          information
-        </div>
-        // <button className="btn btn-primary pull-right">
-        //   Save
-        // </button>
+    <div className="toolbar-actions">
+      <div style={{backgroundColor: '#333', width: "80%", height: "80%", display: 'inline'}}>
+        information
       </div>
-    </footer>
+      <button className="btn btn-primary pull-right">
+        Save
+      </button>
+    </div>
   );
 }
