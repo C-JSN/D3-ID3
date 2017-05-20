6b1d9009c2840ee9801d1664979c1295d3869752
Steven Meads <sbmeads@gmail.com>
2017-05-20 12:06:29 -0700

    change header color to #fff

--- src/components/editor/textEditor.js
+++ src/components/editor/textEditor.js
@@ -146,14 +146,22 @@ class TextEditor extends Component {
         <div className="webview-container">
           <header className="toolbar toolbar-header renderer-header">
             <span id="render-subheader">Renderer</span>
-            <button className="btn btn-default pop-render-btn pull-right">
+            <button className="btn btn-primary pop-window-btn pull-right">
               <span className="icon icon-popup icon-text"></span>
-              Pop Renderer
             </button>
           </header>
           <webview id="render-window" src={this.state.url}></webview>
         </div>
-        <div id="editor"></div>
+        <div className="editor-container">
+          <header className="toolbar toolbar-header renderer-header">
+            <span id="render-subheader">Editor</span>
+            <button className="btn btn-primary pop-window-btn pull-right">
+              <span className="icon icon-popup icon-text"></span>
+            </button>
+          </header>
+          <div id="editor">
+          </div>
+        </div>
       </div>
     );
   }
